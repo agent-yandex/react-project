ee8fc90679ab6f7d69c9f09bd2534e1905417ed8
agent-yandex <fox.online12@gmail.com>
2024-12-02 21:50:41 +0300

    feat: minor changes

---
+++ src/layouts/main-layout/MainLayout.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Outlet, Link, useNavigate } from 'react-router-dom';
+import { Button } from '@consta/uikit/Button';
+import { getToken, dropToken } from '../../services/Tocken';
+import './MainLayout.css';
+
+const MainLayout = () => {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        dropToken();
+        navigate("/");
+    };
+
+    return (
+        <div className="layout">
+            <header className="header">
+                <nav className="nav">
+                    <div className="nav-links">
+                        <Button label="Главная страница" view="secondary" as={Link} to="/" />
+                        <Button label="Услуги компании" view="secondary" as={Link} to="/services" />
+                    </div>
+                    <div className="auth-links">
+                        {
+                            getToken() ? (
+                                <>
+                                    <Link to='/profile'>ФИО</Link>
+                                    <Button label="Выйти" view="primary" onClick={handleLogout} />
+                                </>
+                            ) : (
+                                <Button label="Вход" view="primary" as={Link} to="/login" />
+                            )
+                        }
+                    </div>
+                </nav>
+            </header>
+
+            <main className="main">
+                <Outlet />
+            </main>
+
+            <footer className="footer">
+                <div className="footer-links">
+                    <Button label="Главная страница" view="secondary" as={Link} to="/" />
+                    <Button label="Услуги компании" view="secondary" as={Link} to="/services" />
+                </div>
+                <div className="copyright">
+                    © 2024 Company
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default MainLayout;
